6cda1e3847e01cbd05984b9802c435365e5ad6a3
Biegal <marcin.biegala@gmail.com>
2014-05-20 22:37:13 +0200

    Basic application - without any modifications

---
+++ Gentleman/Gentleman/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Gentleman
+{
+    public class MainViewModel
+    {
+        public string Name { get; set; }
+
+        public string Result { get; set; }
+
+        private ICommand _getResultCommand;
+        public ICommand GetResultCommand
+        {
+            get { return _getResultCommand ?? (_getResultCommand = new RelayCommand(o => GetResult())); }
+        }
+
+        public void GetResult()
+        {
+            Result = String.Empty;
+            if (!String.IsNullOrEmpty(Name))
+            {
+                Result = String.Format("Cześć, {0} !", Name);
+            }
+        }
+    }
+}
