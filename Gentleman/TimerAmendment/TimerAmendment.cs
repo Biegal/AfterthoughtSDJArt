de6028db158ab360b4a3a7742eabef440c785bed
Biegal <marcin.biegala@gmail.com>
2014-05-21 06:31:01 +0200

    TimerAmendment definition

---
+++ Gentleman/TimerAmendment/TimerAmendment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Afterthought;
+
+namespace TimerAmendment
+{
+    public class TimerAmendment<T> : Amendment<T, T>
+    {
+        public TimerAmendment()
+        {
+            Methods
+                .Where(m => m.MethodInfo.Name == "GetResult")
+                .Before(Timer<T>.LogMethodBefore)
+                .After(Timer<T>.LogMethodAfter);
+        }
+    }
+
+    public static class Timer<T>
+    {
+        private static readonly Dictionary<T, Stopwatch> _timers = new Dictionary<T, Stopwatch>();
+
+        public static void LogMethodBefore(T instance, string name, object[] ps)
+        {
+            _timers[instance] = Stopwatch.StartNew();
+        }
+
+        public static void LogMethodAfter(T instance, string name, object[] ps)
+        {
+            var stopwatch = _timers[instance];
+            stopwatch.Stop();
+
+            Console.WriteLine("Czas wykonania: " + stopwatch.Elapsed);
+        }
+    }
+}
---
+++ Gentleman/TimerAmendment/TimerAmendmentAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Afterthought;
+
+namespace TimerAmendment
+{
+    public class TimerAmendmentAttribute : Attribute, IAmendmentAttribute
+    {
+        IEnumerable<ITypeAmendment> IAmendmentAttribute.GetAmendments(Type target)
+        {
+            yield return (ITypeAmendment)typeof(TimerAmendment<>)
+                            .MakeGenericType(target)
+                            .GetConstructor(Type.EmptyTypes)
+                            .Invoke(new object[0]);
+
+        }
+    }
+}
